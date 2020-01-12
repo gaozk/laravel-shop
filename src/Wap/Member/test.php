7e5be60fd3923fddad5d9649ff65f9c986de7ab9
gaozhaokun-mac <gaozhaokun@126.com>
2020-01-12 12:08:08 +0800

    init module laravel shop

---
+++ src/Wap/Member/test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhaokungao
+ * Date: 2020-01-12
+ * Time: 11:33
+ */
+
+namespace Gaozk/LaravelShop/Wap/Member;
+
+class Test{
+    public function index()
+    {
+        return 'test';
+    }
+}
